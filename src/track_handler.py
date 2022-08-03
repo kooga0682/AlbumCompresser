c4fceae14a6fa4b2cf070dfaf27e93645f796f5b
kooga0682 <kooga0682@gmail.com>
2022-08-03 18:04:39 +0900

    [feat] Add compress and remove process for wav/aif

--- src/track_handler.py
+++ src/track_handler.py
@@ -1,13 +1,15 @@
 
-from email.mime import audio
 import subprocess
+import ffmpeg
 import os
+import shutil
+
 
 def sort(track_path):
     extention = track_path.suffix
     # print(extention)
 
-    if extention == ".aif" or extention==".wav":
+    if extention==".aif" or extention==".wav":
         compress_to_flac(track_path)
     elif extention == ".mp4":
         convert_to_m4a(track_path)
@@ -15,7 +17,14 @@ def sort(track_path):
 
 
 def compress_to_flac(track_path):
-    print("compress ",track_path)
+    compressed_audio_path = track_path.with_suffix(".flac")
+    print("compress ",track_path," to ",compressed_audio_path)
+
+    stream = ffmpeg.input(str(track_path))
+    stream = ffmpeg.output(stream, str(compressed_audio_path))
+    # ffmpeg.run(stream, overwrite_output=True)
+    move_to_trashbox(track_path)
+    # subprocess.run("ffmpeg -i "+track_path + ' ' + basename + ".flac", shell=True)
     return
 
 
@@ -26,4 +35,20 @@ def convert_to_m4a(track_path):
     # print(audio_filename)
     # print(type(audio_filename))
     os.rename(track_path, audio_file_path)
+    return
+
+
+def move_to_trashbox(track):
+    trashbox_path = track.parent.parent.parent.joinpath(".trashbox")
+    create_trashbox(trashbox_path)
+
+    moved = shutil.move(track, trashbox_path)
+
+    # print()
+
+    return
+
+
+def create_trashbox(trashbox_path):
+    trashbox_path.mkdir(exist_ok=True)
     return
